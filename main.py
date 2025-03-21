0b309cc8c795184014b306f523873d43ce975949
rs-saran <rs.saran.reddy@gmail.com>
2025-03-21 22:41:18 +0530

    fix server reload

--- main.py
+++ main.py
@@ -186,4 +186,4 @@ def handle_message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
