fc99cab84233243e2154cf0cf9c2a605633bd78f
rs-saran <rs.saran.reddy@gmail.com>
2025-05-02 16:35:11 +0530

    remove reminder and followup flow reliance on exchange

--- src/chat_flows/reminder_flow.py
+++ src/chat_flows/reminder_flow.py
@@ -4,7 +4,7 @@ from src.chat_flows.chat_flow import ChatFlow
 from src.logger import get_logger
 from src.managers.prompt_manager import get_sakha_char_prompt
 from src.response_templates.sakha_template import SakhaResponseForRemFlow
-from src.utils import get_current_time_ist
+from src.utils import get_current_time_ist, exchanges_pretty
 
 logger = get_logger(__name__)
 
@@ -12,57 +12,49 @@ logger = get_logger(__name__)
 class ReminderFlow(ChatFlow):
 
     def generate_response(
-        self,
-        user_id, thread_id,
-        exchange,
-        user_input,
-        latest_exchanges_pretty,
-        user_info,
-        conversation_summary="",
-        activity_details=None,
+        self, conversation_state
     ):
+        user_input = conversation_state.get("user_input")
+        user_info = conversation_state.get("user_info", "no user_info")
+        reminder_start = conversation_state.get("reminder_start", "no user_info")
+
+        latest_exchanges = conversation_state.get("latest_exchanges", [])
+        latest_exchanges_pretty = exchanges_pretty(latest_exchanges)
+        conversation_summary = conversation_state.get("conversation_summary", "")
+        activity_details = conversation_state.get("activity_details", "")
+
         try:
             logger.info(
                 f"Generating reminder response for activity: {activity_details}"
             )
+            chat_prompt_msgs = [
+                SystemMessage(get_sakha_char_prompt()),
+                SystemMessage(f"Current time: {get_current_time_ist()}"),
+                SystemMessage(f"User Info: {user_info}")]
 
-            if exchange == 0:
-                chat_prompt_msgs = [
-                    SystemMessage(get_sakha_char_prompt()),
-                    SystemMessage(f"Current time: {get_current_time_ist()}"),
-                    SystemMessage(
-                        f"Conversation History:\n{latest_exchanges_pretty}"
-                    ),
-                    SystemMessage(
-                        f"Previously, you set a reminder for an activity for the user. It’s time! "
-                        f"{activity_details}"
-                        f"Encourage the user to start their activity with enthusiasm and motivation."
-                    ),
-                ]
+            if conversation_summary == "":
+                chat_context = f"Conversation History:\n{latest_exchanges_pretty}"
             else:
-                chat_prompt_msgs = [
-                    SystemMessage(get_sakha_char_prompt()),
-                    SystemMessage(f"Current time: {get_current_time_ist()}"),
-                    SystemMessage(
-                        f"Conversation History:\n{latest_exchanges_pretty}"
-                    ),
-                    SystemMessage(
-                        f"{activity_details}. Motivate the user to complete the activity. "
-                        f"If they seem reluctant, suggest small fun modifications to make it more enjoyable "
-                        f"or provide alternatives. Otherwise, end the conversation."
-                    ),
-                    HumanMessage(user_input),
-                ]
+                chat_context = f"Conversation History:<conversation_history><summary>{conversation_summary}</summary> <latest_exchanges> {latest_exchanges_pretty} </latest_exchanges> </conversation_history>"
+
+            chat_prompt_msgs.append(SystemMessage(chat_context))
+
+            if reminder_start:
+                reminder_instruction = f"Previously, you set a reminder for an activity for the user. It’s time!\n{activity_details}\nEncourage the user to start their activity with enthusiasm and motivation."
+                chat_prompt_msgs.append(SystemMessage(reminder_instruction))
+            else:
+                reminder_instruction = f"{activity_details}. \n Motivate the user to complete the activity. If they seem reluctant, suggest small fun modifications to make it more enjoyable or ask if they want alternatives. Otherwise, end the conversation."
+                chat_prompt_msgs.extend([SystemMessage(reminder_instruction), HumanMessage(user_input)])
 
             model_response = self.llm.with_structured_output(
                 SakhaResponseForRemFlow
             ).invoke(chat_prompt_msgs)
+            conversation_state.update(latest_sakha_response=model_response, latest_rem_flow_response=model_response)
+
             logger.info("Successfully generated reminder response.")
-            return model_response
+
+            return conversation_state
 
         except Exception as e:
             logger.error(f"Error generating reminder response: {str(e)}", exc_info=True)
-            return {
-                "replyToUser": "Sorry, I ran into an issue. Can you try again?",
-                "error": f"Error generating response in RemFlow",
-            }
+            return conversation_state
