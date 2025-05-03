949f9f18c0a5e990d0675f991b292511c758c1ad
rs-saran <rs.saran.reddy@gmail.com>
2025-05-03 10:30:46 +0530

    fixed conv hist update logic

--- src/chat_flows/follow_up_flow.py
+++ src/chat_flows/follow_up_flow.py
@@ -63,7 +63,7 @@ class FollowUpFlow(ChatFlow):
             ).invoke(chat_prompt_msgs)
             logger.info("Successfully generated follow-up response.")
             conversation_state.update(latest_sakha_response=model_response, latest_fu_flow_response=model_response,
-                                      followup_start=False, flow="follow_up")
+                                      flow="follow_up")
 
             return conversation_state
 
